df7f9e92582a12b85eb4685d4e69f32f44fd339a
manojmanu4 <87852510+manojmanu4@users.noreply.github.com>
2021-08-06 15:17:01 +0530

    Add files via upload
    updated payment pages

---
+++ TxnTest.php
@@ -0,0 +1,76 @@
+<?php
+	header("Pragma: no-cache");
+	header("Cache-Control: no-cache");
+	header("Expires: 0");
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Payment Page</title>
+<style>
+body {
+    background-image: linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(assets/images/bg1.jpg);
+    background-position: center;
+    background-size: cover;
+}
+</style>
+<meta name="GENERATOR" content="Evrsoft First Page">
+</head>
+<body>
+	<h1 style="margin-top: 180px; text-align: center;">Company Name</h1>
+	<pre>
+	</pre>
+	<center>
+		<form method="post" action="pgRedirect.php" style="margin-top: -12px;">
+		<table border="5">
+			<tbody>
+       <tr>
+					<th>S.No</th>
+					<th>Label</th>
+					<th>Value</th>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td><label>ORDER_ID</label></td>
+					<td><input id="ORDER_ID" tabindex="1" maxlength="20" size="20"
+						name="ORDER_ID" autocomplete="off"
+						value="<?php echo  "ORDS" . rand(10000,99999999)?>">
+					</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td><label>CUSTID</label></td>
+					<td><input id="CUST_ID" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="CUST001"></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td><label>INDUSTRY_TYPE_ID</label></td>
+					<td><input id="INDUSTRY_TYPE_ID" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="Retail"></td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td><label>Channel</label></td>
+					<td><input id="CHANNEL_ID" tabindex="4" maxlength="12"
+						size="12" name="CHANNEL_ID" autocomplete="off" value="WEB">
+					</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td><label>txnAmount</label></td>
+					<td><input title="TXN_AMOUNT" tabindex="10"
+						type="text" name="TXN_AMOUNT"
+						value="1">
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td><input value="PAY NOW" type="submit"	onclick=""></td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
+</center>
+</body>
+</html>
